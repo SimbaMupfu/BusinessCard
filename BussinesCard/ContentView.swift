6bb0cbf1ec7e52679df5842f39444b642c989500
simba_mupfu <scmupfururirwa@gmail.com>
2023-07-01 14:29:02 +0200

    Add background colour

--- BussinesCard/ContentView.swift
+++ BussinesCard/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            Text("Simbarashe Mupfururirwa")
         }
-        .padding()
     }
 }
 
