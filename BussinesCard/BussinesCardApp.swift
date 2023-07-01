e8dff3286fe22ebf46949469db8a32c2b9d34f7b
simba_mupfu <scmupfururirwa@gmail.com>
2023-07-01 14:21:22 +0200

    Initial commit

---
+++ BussinesCard/BussinesCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BussinesCardApp.swift
+//  BussinesCard
+//
+//  Created by Simbarashe Mupfururirwa on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct BussinesCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
